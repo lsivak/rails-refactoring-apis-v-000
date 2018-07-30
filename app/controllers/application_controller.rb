dcd80744d22ed3f191cc8f5c7a993b88cd59e2e1
Lara Sanders <larasanders@gmail.com>
2018-07-30 15:26:13 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
     def authenticate_user
        client_id = ENV['GITHUB_CLIENT']
-      redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
+      redirect_to "https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=repo" if !logged_in?
     end
 
     def logged_in?
